eacc930341780ef4594763f465216d645472356d
delta-kor <contact@izflix.net>
2023-07-09 12:03:09 +0900

    Add lyrics animation

--- components/Lyrics.tsx
+++ components/Lyrics.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
 import { Nanum_Gothic } from 'next/font/google';
 
 const font = Nanum_Gothic({ subsets: ['latin'], weight: ['400', '800'] });
@@ -15,20 +18,61 @@ export default function Lyrics({ music, lyrics, time }: Props) {
   const keys = Object.keys(lyrics).map(Number);
 
   let nextKeyIndex = keys.findIndex((key) => key > timeMs);
-  nextKeyIndex = nextKeyIndex < 0 ? keys.length - 1 : nextKeyIndex;
+  nextKeyIndex = nextKeyIndex < 0 ? keys.length : nextKeyIndex;
 
   const currentKeyIndex = nextKeyIndex - 1;
   const currentLyrics = lyrics[keys[currentKeyIndex]];
   const nextLyrics = lyrics[keys[nextKeyIndex]];
 
+  const previousKeyIndex = nextKeyIndex - 2;
+  const previousLyrics = lyrics[keys[previousKeyIndex]];
+
+  const secondNextKeyIndex = nextKeyIndex + 1;
+  const secondNextLyrics = lyrics[keys[secondNextKeyIndex]];
+
   return (
-    <div className={`flex flex-col py-xs justify-center items-center gap-xs ${font.className}`}>
-      <div className="text-[16px] font-bold text-white text-left self-stretch truncate">
-        {currentLyrics || `${music.title} - ${music.group}`}
-      </div>
-      <div className="text-[16px] text-white text-left self-stretch opacity-50 truncate">
-        {nextLyrics}
-      </div>
+    <div className={`relative py-xs h-[60px] overflow-y-hidden ${font.className}`}>
+      <AnimatePresence>
+        {previousLyrics && (
+          <motion.div
+            key={previousKeyIndex}
+            layoutId={previousKeyIndex.toString()}
+            initial={{ opacity: 0.5 }}
+            animate={{ opacity: 0 }}
+            className="absolute top-[-24px] left-[0] w-[100%] text-[16px] font-bold text-white text-left truncate"
+          >
+            {previousLyrics}
+          </motion.div>
+        )}
+        <motion.div
+          key={currentKeyIndex}
+          layoutId={currentKeyIndex.toString()}
+          initial={{ opacity: 0.5 }}
+          animate={{ opacity: 1 }}
+          className="mb-xs text-[16px] font-bold text-white text-left truncate"
+        >
+          {currentLyrics || `${music.title} - ${music.group}`}
+        </motion.div>
+        <motion.div
+          key={nextKeyIndex}
+          layoutId={nextKeyIndex.toString()}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.5 }}
+          className="text-[16px] text-white text-left opacity-50 truncate"
+        >
+          {nextLyrics}
+        </motion.div>
+        {secondNextLyrics && (
+          <motion.div
+            key={secondNextKeyIndex}
+            layoutId={secondNextKeyIndex.toString()}
+            initial={{ opacity: 0 }}
+            className="text-[16px] font-bold text-white text-left self-stretch truncate"
+          >
+            {secondNextLyrics}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
