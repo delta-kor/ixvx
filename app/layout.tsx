3c0fd6e491e3266d07052e10bdd7b7bad62c234f
delta-kor <contact@izflix.net>
2023-07-23 16:45:11 +0900

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,21 @@
 import './globals.css';
 import { Montserrat } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const font = Montserrat({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {children}
+        <Analytics
+          beforeSend={(event) => {
+            if (!event.url.includes('skycon.kr')) return null;
+            return event;
+          }}
+        />
+      </body>
     </html>
   );
 }
--- components/SessionInfo.tsx
+++ components/SessionInfo.tsx
@@ -9,17 +9,15 @@ interface Props {
 
 export default function SessionInfo({ music: { title, group, album } }: Props) {
   return (
-    <div className="flex px-xl py-md items-center gap-md lg:px-lg lg:py-lg cursor-pointer select-none">
-      <Image src={album} alt={title} className="w-[54px] h-[54px] flex-shrink-0 rounded-sm" />
-      <div className="flex flex-col justify-center gap-2xs flex-grow min-w-0">
-        <div className="text-heading3 text-white truncate">{title}</div>
-        <div className="text-body1 text-white truncate">{group}</div>
-      </div>
-      <Link href="/">
-        <Jelly small>
-          <Icon type="chevron_right" className="w-xl h-xl text-primary" />
-        </Jelly>
-      </Link>
-    </div>
+    <Link href="/">
+      <Jelly className="flex px-xl py-md items-center gap-md lg:px-lg lg:py-lg cursor-pointer select-none">
+        <Image src={album} alt={title} className="w-[54px] h-[54px] flex-shrink-0 rounded-sm" />
+        <div className="flex flex-col justify-center gap-2xs flex-grow min-w-0">
+          <div className="text-heading3 text-white truncate">{title}</div>
+          <div className="text-body1 text-white truncate">{group}</div>
+        </div>
+        <Icon type="chevron_right" className="w-xl h-xl text-primary" />
+      </Jelly>
+    </Link>
   );
 }
